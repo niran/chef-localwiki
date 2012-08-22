bda3f0a15c8cd8b118a49700de3a6b27c9f8c2b3
Niran Babalola <niran@niran.org>
2012-08-22 14:30:04 -0500

    Create localwiki virtualenv

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,8 @@
 include_recipe "geodjango"
+include_recipe "python::pip"
+include_recipe "python::virtualenv"
+
+python_virtualenv "/usr/lib/virtualenvs/localwiki" do
+  action :create
+  options "--system-site-packages"
+end
