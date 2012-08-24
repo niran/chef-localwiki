5852fbad76699851f6beca26ab830529b68fa191
Niran Babalola <niran@niran.org>
2012-08-24 17:43:11 -0500

    Add packages for the remainng localwiki requirements and git

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,11 @@ include_recipe "geodjango"
 include_recipe "python::pip"
 include_recipe "python::virtualenv"
 
+package "git"
+package "python-imaging"
+package "python-lxml"
+package "solr-jetty"
+
 python_virtualenv "/usr/lib/virtualenvs/localwiki" do
   action :create
   options "--system-site-packages"
